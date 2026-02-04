e365f20680eef99a070c57934f74c0ae144321ad
Clément Baillet <c.bailletpro@icloud.com>
2026-02-05 00:18:08 +0100

    version cuda

--- PoissonCuda/src/cuda/iteration.cu
+++ PoissonCuda/src/cuda/iteration.cu
@@ -1,17 +1,60 @@
-#include "dim.cuh"
 #include "cuda_check.cuh"
+#include "dim.cuh"
 #include "user.cuh"
 
-#include "timer.hxx"
 #include "iteration.hxx"
+#include "timer.hxx"
 
-// A completer : definition du noyau
+#include "cuda_check.cuh"
+#include "dim.cuh"
+#include "user.cuh"
 
-void iteration(
-    Values & v, Values & u, double dt, int n[3],
-    int imin, int imax, 
-    int jmin, int jmax,
-    int kmin, int kmax)
-{
-// A completer : appel du noyau
+#include "iteration.hxx"
+#include "timer.hxx"
+
+__global__ void iteration_kernel(double *v, const double *u, double dt,
+                                 int imin, int imax, int jmin, int jmax,
+                                 int kmin, int kmax) {
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  int j = threadIdx.y + blockIdx.y * blockDim.y;
+  int k = threadIdx.z + blockIdx.z * blockDim.z;
+
+  if (i >= imin && i <= imax && j >= jmin && j <= jmax && k >= kmin &&
+      k <= kmax) {
+    int p = i + d_n[0] * (j + d_n[1] * k);
+
+    // Indices of neighbors
+    int p_im1 = p - 1;
+    int p_ip1 = p + 1;
+    int p_jm1 = p - d_n[0];
+    int p_jp1 = p + d_n[0];
+    int p_km1 = p - d_n[0] * d_n[1];
+    int p_kp1 = p + d_n[0] * d_n[1];
+
+    double lap = (u[p_im1] + u[p_ip1] - 2.0 * u[p]) * d_lambda[0] +
+                 (u[p_jm1] + u[p_jp1] - 2.0 * u[p]) * d_lambda[1] +
+                 (u[p_km1] + u[p_kp1] - 2.0 * u[p]) * d_lambda[2];
+
+    // Coordinates for force
+    double x = d_xmin[0] + i * d_dx[0];
+    double y = d_xmin[1] + j * d_dx[1];
+    double z = d_xmin[2] + k * d_dx[2];
+
+    double f = force(x, y, z);
+
+    v[p] = u[p] + dt * (lap + f);
+  }
+}
+
+void iteration(Values &v, Values &u, double dt, int n[3], int imin, int imax,
+               int jmin, int jmax, int kmin, int kmax) {
+  dim3 dimBlock(8, 8, 8);
+  dim3 dimGrid((n[0] + dimBlock.x - 1) / dimBlock.x,
+               (n[1] + dimBlock.y - 1) / dimBlock.y,
+               (n[2] + dimBlock.z - 1) / dimBlock.z);
+
+  iteration_kernel<<<dimGrid, dimBlock>>>(v.dataGPU(), u.dataGPU(), dt, imin,
+                                          imax, jmin, jmax, kmin, kmax);
+  cudaDeviceSynchronize();
+  CUDA_CHECK_KERNEL();
 }
--- PoissonCuda/src/cuda/user.cu
+++ PoissonCuda/src/cuda/user.cu
@@ -1,2 +1,26 @@
+#include "user.cuh"
+#include <cmath>
 
-// A completer
+__device__
+double cond_ini(double x, double y, double z)
+{
+  x -= 0.5;
+  y -= 0.5;
+  z -= 0.5;
+  if (x * x + y * y + z * z < 0.1)
+    return 1.0;
+  else
+    return 0.0;
+}
+
+__device__
+double cond_lim(double x, double y, double z)
+{
+  return 0.0;
+}
+
+__device__
+double force(double x, double y, double z)
+{
+  return sin(x - 0.5) * cos(y - 0.5) * exp(-z * z);
+}
