1e272c6612d9d4506de08e7222719529a0734206
Clément Baillet <c.bailletpro@icloud.com>
2026-02-06 10:14:47 +0100

    version cuda

--- PoissonCuda/src/cuda/dim.cu
+++ PoissonCuda/src/cuda/dim.cu
@@ -1,5 +1,26 @@
 #include "dim.cuh"
 
-// Content moved to iteration.cu to resolve symbol visibility issues with
-// separate compilation. This file is kept to avoid breaking build scripts that
-// expect it, or can be removed if CMake allows.
+__constant__ int d_n[3];
+__constant__ double d_xmin[3];
+__constant__ double d_dx[3];
+__constant__ double d_lambda[3];
+
+void setDims(const int *h_n, const double *h_xmin, const double *h_dx,
+             const double *h_lambda) {
+  Timer &T = GetTimer(5);
+  T.start();
+
+  // Use cudaMemcpyToSymbol directly now that we are in the same file/module as
+  // the definition or stick to the safer cudaGetSymbolAddress approach if
+  // desired, but direct use is standard if visibility is good. We'll stick to
+  // the safer approach causing the least friction, but since we are DEFINING it
+  // here:
+
+  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_n, h_n, 3 * sizeof(int)));
+  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_xmin, h_xmin, 3 * sizeof(double)));
+  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_dx, h_dx, 3 * sizeof(double)));
+  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_lambda, h_lambda, 3 * sizeof(double)));
+
+  cudaDeviceSynchronize();
+  T.stop();
+}
--- PoissonCuda/src/cuda/iteration.cu
+++ PoissonCuda/src/cuda/iteration.cu
@@ -13,31 +13,6 @@
 #include "iteration.hxx"
 #include "timer.hxx"
 
-__constant__ int d_n[3];
-__constant__ double d_xmin[3];
-__constant__ double d_dx[3];
-__constant__ double d_lambda[3];
-
-void setDims(const int *h_n, const double *h_xmin, const double *h_dx,
-             const double *h_lambda) {
-  Timer &T = GetTimer(5);
-  T.start();
-
-  // Use cudaMemcpyToSymbol directly now that we are in the same file/module as
-  // the definition or stick to the safer cudaGetSymbolAddress approach if
-  // desired, but direct use is standard if visibility is good. We'll stick to
-  // the safer approach causing the least friction, but since we are DEFINING it
-  // here:
-
-  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_n, h_n, 3 * sizeof(int)));
-  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_xmin, h_xmin, 3 * sizeof(double)));
-  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_dx, h_dx, 3 * sizeof(double)));
-  CUDA_CHECK_OP(cudaMemcpyToSymbol(d_lambda, h_lambda, 3 * sizeof(double)));
-
-  cudaDeviceSynchronize();
-  T.stop();
-}
-
 __global__ void iteration_kernel(double *v, const double *u, double dt,
                                  int imin, int imax, int jmin, int jmax,
                                  int kmin, int kmax) {
